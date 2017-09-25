c30bd93b3cbb2fd06307b936229609aea0fd60b7
Eric Jonese <ericnatejones@gmail.com>
2017-09-25 16:59:58 -0600

    line moves according to how quickly you move the slider

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let sliderValue = 0;
 let prevSliderValue = 0;
 let currentSpeed = sliderValue - prevSliderValue;
+let line = document.getElementById("line");
+let speed = currentSpeed;
+line.style.right = "100px"
 
 document.slider.momentum.addEventListener("input", event=>{
   sliderValue = parseInt(event.target.value);
@@ -8,9 +11,19 @@ document.slider.momentum.addEventListener("input", event=>{
 })
 
 setInterval(()=>{
+
   currentSpeed = sliderValue - prevSliderValue;
   prevSliderValue = sliderValue;
   if (sliderValue === 100){
-    console.log(currentSpeed);
+    speed = currentSpeed;
+    console.log(sliderValue)
+    sliderValue = 99;
+    document.getElementById("slide-input").value = "99";
+
   }
+  let linePosition = parseInt(line.style.right.slice(0, line.style.right.length - 2));
+
+  linePosition = (linePosition + speed) + 'px';
+  line.style.right = linePosition;
+
 }, 1)
